c9d44688faa7007c9ed11ca2952775273f238b6c
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2021-11-07 22:18:58 +0700

    Comment test

--- src/components/elements/Button/index.test.tsx
+++ src/components/elements/Button/index.test.tsx
@@ -1,9 +1,9 @@
 import {} from '.'
 
-describe('<Button />', () => {
-  it('test', () => {
-    expect.assertions(1)
+// describe('<Button />', () => {
+//   it('test', () => {
+//     expect.assertions(1)
 
-    expect(true).toBe(true)
-  })
-})
+//     expect(true).toBe(true)
+//   })
+// })
