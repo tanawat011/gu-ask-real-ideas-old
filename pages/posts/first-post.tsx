6a081fc05456d31101f5babc694692c085242d99
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2021-11-07 18:38:30 +0700

    Add eslint and prettier

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head'
+import Link from 'next/link'
 
 const FirstPost = () => {
   return (
@@ -9,22 +9,22 @@ const FirstPost = () => {
       </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">
+        <Link href='/'>
           <a>Back to home</a>
         </Link>
       </h2>
 
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0">
+      <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
+        <div className='flex-shrink-0'>
           {/* <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"> */}
         </div>
         <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-gray-500">You have a new message!</p>
+          <div className='text-xl font-medium text-black'>ChitChat</div>
+          <p className='text-gray-500'>You have a new message!</p>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default FirstPost;
+export default FirstPost
